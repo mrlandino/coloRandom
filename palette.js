87479a7a2711d7f8f485d3a008356e0029c7be36
Eli Davidson <eli@shadowlab.dev>
2022-02-25 15:41:11 -0700

    Refactor js

--- palette.js
+++ palette.js
@@ -1,8 +1,9 @@
 class Palette {
     constructor () {
         this.palette = this.createPalette();
-        this.id= Date.now()
+        this.id= Date.now();
     }
+
     createPalette() {
         var palette = [];
         for (var i =0; i < 5; i++) {
@@ -10,15 +11,15 @@ class Palette {
         }
         return palette;
     }
+
     refresh() {
-        // console.log(this.palette)
         for(var i = 0; i<5; i++) {
-            // console.log(this.palette[i].locked)
             if (!this.palette[i].locked) {
                 this.palette.splice(i, 1, new Color());
             }
         }
-      }
+    }
+
     toggleLock(lockId) {
           this.palette[lockId].locked = !this.palette[lockId].locked;
         }
